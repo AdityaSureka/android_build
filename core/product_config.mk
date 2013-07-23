8d9c8a95b6fc83b10e109e1d3bbc7b7c4d311f50
itsmikeramsay <ronaldramsayii@gmail.com>
2013-07-23 10:48:06 -0400

    Prepping Illusion Project...

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,16 +185,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(UKG_BUILD),)
-    all_product_configs := $(shell ls device/*/$(UKG_BUILD)/ukg.mk)
+  ifneq ($(ILLUSION_BUILD),)
+    all_product_configs := $(shell ls device/*/$(ILLUSION_BUILD)/illusion.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # UKG_BUILD
+  endif # ILLUSION_BUILD
 endif
 
-ifeq ($(UKG_BUILD),)
+ifeq ($(ILLUSION_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
